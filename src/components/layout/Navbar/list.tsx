efb3d70e51c367fd47920961ab41ba155a7f80d3
Md.Rabiul Hasan <70057377+md-rabiul-hasan@users.noreply.github.com>
2025-02-12 14:31:28 +0600

    first commit

---
+++ src/components/layout/Navbar/list.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from 'react';
+import { AiOutlineDashboard as DashboardIcon } from 'react-icons/ai';
+import { FaProductHunt } from "react-icons/fa";
+
+type MenuItem = {
+  link: string
+  label: string
+  icon: ReactNode
+}
+
+type MenuItemWithoutIcon = {
+  link: string
+  label: string
+}
+
+type MenuWithLinks = {
+  label: string
+  icon: ReactNode
+  links: MenuItemWithoutIcon[]
+}
+
+type MenuItems = MenuItem | MenuWithLinks
+
+export const menuItems = (roles: string[]) => [
+  { link: '/', label: 'Dashboard', icon: <DashboardIcon /> },
+  {
+    label: 'Product',
+    icon: <FaProductHunt />,
+    links: [
+      { link: '/products/list', label: 'List' },
+    ]
+  },
+]
+
+export const isActiveLink = (path: string, link: string = ''): boolean => {
+  if (link === '/') return path === link
+
+  const nextChar = path[link.length]
+  return path.startsWith(link) && (!nextChar || nextChar === '/')
+}
+
+export const isMenuWithLinks = (item: MenuItems): item is MenuWithLinks => (item as MenuWithLinks).links !== undefined
